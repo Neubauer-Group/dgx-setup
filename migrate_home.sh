1ac89f8512e87b4cf5a5015799a6fbaa93b71da0
Matthew Feickert <matthew.feickert@cern.ch>
2021-07-12 15:34:36 -0500

    feat: Ensure .bash_aliases and .bashrc_user exist (#3)
    * Use touch to ensure .bash_aliases and .bashrc_user exist and are symlinked back to /home/${USER}
    * Also symlink .Xauthority
    * Add sourcing of .bash_aliases and .bashrc_user in .bashrc as part of migrate_home.sh

--- migrate_home.sh
+++ migrate_home.sh
@@ -7,24 +7,31 @@ mkdir -p "${RAID_HOME}"
 cd "${RAID_HOME}"
 
 
-cp "/home/${USER}/".bash* .
-# symlink Bash files from home on /raid to old home
+cp "${DEFAULT_HOME}/".bash* .
+# symlink Bash files from RAID_HOME to DEFAULT_HOME so still usable
 ln --symbolic --force $(readlink -f "${RAID_HOME}/.bash_profile") $(readlink -f "${DEFAULT_HOME}/.bash_profile")
 ln --symbolic --force $(readlink -f "${RAID_HOME}/.bashrc") $(readlink -f "${DEFAULT_HOME}/.bashrc")
 ln --symbolic --force $(readlink -f "${RAID_HOME}/.bash_logout") $(readlink -f "${DEFAULT_HOME}/.bash_logout")
 
-if [ -f "${RAID_HOME}/.bashrc_user"]; then
-  ln --symbolic --force $(readlink -f "${RAID_HOME}/.bashrc_user") $(readlink -f "${DEFAULT_HOME}/.bashrc_user")
-fi
-if [ -f "${RAID_HOME}/.bash_aliases"]; then
-  ln --symbolic --force $(readlink -f "${RAID_HOME}/.bash_aliases") $(readlink -f "${DEFAULT_HOME}/.bash_aliases")
-fi
+# Ensure .bash_aliases and .bashrc_user exist
+touch "${RAID_HOME}/.bash_aliases"
+ln --symbolic --force $(readlink -f "${RAID_HOME}/.bash_aliases") $(readlink -f "${DEFAULT_HOME}/.bash_aliases")
+touch "${RAID_HOME}/.bashrc_user"
+ln --symbolic --force $(readlink -f "${RAID_HOME}/.bashrc_user") $(readlink -f "${DEFAULT_HOME}/.bashrc_user")
+
+# Also get .Xauthority
+cp "${DEFAULT_HOME}/.Xauthority" .
+ln --symbolic --force $(readlink -f "${RAID_HOME}/.Xauthority") $(readlink -f "${DEFAULT_HOME}/.Xauthority")
 
 # symlink .ssh info from default home to home on /raid
 ln --symbolic $(readlink -f "${DEFAULT_HOME}/.ssh") $(readlink -f "${RAID_HOME}")/.ssh
 
 # Set RAID_HOME as new HOME in .bash_profile
 sed -i '/^# .bash_profile*/a export HOME="/raid/projects/${USER}"' "${RAID_HOME}/.bash_profile"
+
+# Source .bash_aliases and .bashrc_user from .bashrc
+printf '\n# User specific aliases and functions\nif [ -f ~/.bash_aliases ]; then\n    . ~/.bash_aliases\nfi\n' >> "${RAID_HOME}/.bashrc"
+printf "\n# Instead of directly editing the system's default .bashrc load a user version\nif [ -f ~/.bashrc_user ]; then\n    . ~/.bashrc_user\nfi\n" >> "${RAID_HOME}/.bashrc"
 
 unset DEFAULT_HOME
 unset RAID_HOME
