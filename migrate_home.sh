d5102b149d601da703993e6b38fd4f94b41e729a
Matthew Feickert <matthew.feickert@cern.ch>
2021-07-12 14:40:15 -0500

    Aslo get Xauthority

--- migrate_home.sh
+++ migrate_home.sh
@@ -7,7 +7,7 @@ mkdir -p "${RAID_HOME}"
 cd "${RAID_HOME}"
 
 
-cp "/home/${USER}/".bash* .
+cp "${DEFAULT_HOME}/".bash* .
 # symlink Bash files from home on /raid to old home
 ln --symbolic --force $(readlink -f "${RAID_HOME}/.bash_profile") $(readlink -f "${DEFAULT_HOME}/.bash_profile")
 ln --symbolic --force $(readlink -f "${RAID_HOME}/.bashrc") $(readlink -f "${DEFAULT_HOME}/.bashrc")
@@ -19,6 +19,10 @@ fi
 if [ -f "${RAID_HOME}/.bash_aliases"]; then
   ln --symbolic --force $(readlink -f "${RAID_HOME}/.bash_aliases") $(readlink -f "${DEFAULT_HOME}/.bash_aliases")
 fi
+
+# Also get .Xauthority
+cp "${DEFAULT_HOME}/.Xauthority" .
+ln --symbolic --force $(readlink -f "${RAID_HOME}/.Xauthority") $(readlink -f "${DEFAULT_HOME}/.Xauthority")
 
 # symlink .ssh info from default home to home on /raid
 ln --symbolic $(readlink -f "${DEFAULT_HOME}/.ssh") $(readlink -f "${RAID_HOME}")/.ssh
