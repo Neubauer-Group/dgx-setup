21467ef7b881872ff68635bfa65f5530755b1d84
Matthew Feickert <matthew.feickert@cern.ch>
2021-07-27 22:35:04 -0500

    Add python make options

--- build_python.sh
+++ build_python.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+set -o pipefail
+
+# Enable better devtools for compiling as they're available
+source scl_source enable devtoolset-9
+
 export CPYTHON_VERSION=3.8.11
 
 PYTHON_CONFIGURE_OPTS="--with-ensurepip --enable-optimizations --with-lto --enable-loadable-sqlite-extensions --enable-ipv6 --enable-shared" \
+    PYTHON_MAKE_OPTS="-j8" \
     pyenv install --force "${CPYTHON_VERSION}"
 
 unset CPYTHON_VERSION
